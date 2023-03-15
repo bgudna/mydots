e984d648c0df331fc7fabac440417b9d0c27f128
BGUDNA <b@bgudna.com>
2023-03-15 17:30:31 +0100

    bash script to create multiple lockscreens for i3lock

---
+++ scripts/mk-lock-screen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# simple tool to create multiple lockscreens for i3 
+    convert /mnt/drive/pics/locks/*.jpg -font Liberation-Mono \
+    -fill '#0008' -draw 'rectangle 20,110,480,145' \
+    -fill white -pointsize 32  -annotate +30+139 'TYPE PASSWORD TO UNLOCK' \
+    /mnt/drive/pics/locks/output/lockimage.jpg
+
