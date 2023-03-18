e98efa491e7403ff0885125fed06d35b3bc33598
BGUDNA <b@bgudna.com>
2023-03-18 08:12:49 +0100

    more tweaks to neomutt

--- scripts/mk-lock-screen.sh
+++ scripts/mk-lock-screen.sh
@@ -3,5 +3,5 @@
     convert /mnt/drive/pics/locks/*.jpg -font Liberation-Mono \
     -fill '#0008' -draw 'rectangle 20,110,480,145' \
     -fill white -pointsize 32  -annotate +30+139 'TYPE PASSWORD TO UNLOCK' \
-    /mnt/drive/pics/locks/output/lockimage.jpg
+    /mnt/drive/pics/locks/output/lockimage.png
 
